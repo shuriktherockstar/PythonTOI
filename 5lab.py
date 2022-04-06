d905dfc261b42511878602f075c5d1c5eedffbd5
shuriktherockstar <shuriktherockstar@gmail.com>
2022-04-06 19:18:33 +0300

    Отформатировал код согласно PEP8

--- 5lab.py
+++ 5lab.py
@@ -1,15 +1,14 @@
-print("Введите список слов через запятую: ")
-wList = input().split(", ")  # ввод строки
-wLots = set(wList)  # формирование множества
-LotsLength = len(wLots)
-print("Создано множество из {} слов(-а): ".format(LotsLength))
-print(wLots)
+words_list = input("Введите список слов через запятую: ").split(", ")  # ввод строки
+words_lots = set(words_list)  # формирование множества
+lots_length = len(words_lots)
+print("Создано множество из {} слов(-а): ".format(lots_length))
+print(words_lots)
 
-valList = []
-for i in range(LotsLength):
+values_list = []
+for i in range(lots_length):
     print("Дайте толкование {} слова".format(i + 1))
     val = input()
-    valList.append(val)
+    values_list.append(val)
 
-Dict = dict(zip(wLots, valList))
-print("Словарь: {}".format(Dict))
+new_dict = dict(zip(words_lots, values_list))
+print("Словарь: {}".format(new_dict))
